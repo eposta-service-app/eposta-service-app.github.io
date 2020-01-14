8213959f827862aa2d2a96986b5ed5859282581d
ghostfreak3000 <ghostfreak3000>
2020-01-14 04:43:03 +0000

    service_0.0.9

---
+++ precache-manifest.85640bf8c7268a84e7e678122e99a7f7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "283d0503b5b639615e3abe676a6cdfe4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b52bd67ec9b3c6b8af4c",
+    "url": "/static/css/main.9b0dc91e.chunk.css"
+  },
+  {
+    "revision": "588be08b4df94c8512cf",
+    "url": "/static/js/2.0d3b4950.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.0d3b4950.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b52bd67ec9b3c6b8af4c",
+    "url": "/static/js/main.c561c4af.chunk.js"
+  },
+  {
+    "revision": "77f331c275a3e4d506b7",
+    "url": "/static/js/runtime-main.d40a0c2d.js"
+  },
+  {
+    "revision": "237974477c085580a82ed562f555d8cd",
+    "url": "/static/media/untitled-font-1.23797447.woff"
+  },
+  {
+    "revision": "45dcba9a73a9f977eb1cf8631e623a7c",
+    "url": "/static/media/untitled-font-1.45dcba9a.eot"
+  },
+  {
+    "revision": "609f2dd157142e906c8915959fd51882",
+    "url": "/static/media/untitled-font-1.609f2dd1.ttf"
+  },
+  {
+    "revision": "79cad6f4330ac47fb368a1c606fe5f04",
+    "url": "/static/media/untitled-font-1.79cad6f4.svg"
+  }
+]);
